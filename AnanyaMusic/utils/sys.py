6deefaf1b014afb0cc3733dd5bb2ecce81c81dd7
AkashOP <akp954834@gmail.com>
2025-06-28 17:21:58 +0530

    Update sys.py

--- AnanyaMusic/utils/sys.py
+++ AnanyaMusic/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from ShrutiMusic.misc import _boot_
-from ShrutiMusic.utils.formatters import get_readable_time
+from AnanyaMusic.misc import _boot_
+from AnanyaMusic.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
