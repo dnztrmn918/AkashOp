0a291cec486e89f8cb1c52ecfc671ee7d4ab4d1a
AkashOP <akp954834@gmail.com>
2025-06-28 17:12:14 +0530

    Update __main__.py

--- AnanyaMusic/__main__.py
+++ AnanyaMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from ShrutiMusic import LOGGER, app, userbot
-from ShrutiMusic.core.call import Aviax
-from ShrutiMusic.misc import sudo
-from ShrutiMusic.plugins import ALL_MODULES
-from ShrutiMusic.utils.database import get_banned_users, get_gbanned
+from AnanyaMusic import LOGGER, app, userbot
+from AnanyaMusic.core.call import Aviax
+from AnanyaMusic.misc import sudo
+from AnanyaMusic.plugins import ALL_MODULES
+from AnanyaMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,27 +35,27 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("ShrutiMusic.plugins" + all_module)
-    LOGGER("ShrutiMusic.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("AnanyaMusic.plugins" + all_module)
+    LOGGER("AnanyaMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Aviax.start()
     try:
         await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
-        LOGGER("ShrutiMusic").error(
+        LOGGER("AnanyaMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Aviax.decorators()
-    LOGGER("ShrutiMusic").info(
+    LOGGER("AnanyaMusic").info(
         "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("ShrutiMusic").info("Stopping Aviax Music Bot...")
+    LOGGER("AnanyaMusic").info("Stopping Aviax Music Bot...")
 
 
 if __name__ == "__main__":
