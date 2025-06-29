416ac46ee0a3f60e0588e55c4274ce0c124fdd7b
AkashOP <akp954834@gmail.com>
2025-06-29 08:26:28 +0530

    Add files via upload

---
+++ AnanyaMusic/utils/inline/start.py
@@ -0,0 +1,33 @@
+from pyrogram.types import InlineKeyboardButton
+import config
+from ShrutiMusic import app
+
+def start_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🥀ᴀᴅᴅ ᴍᴇ🥀", url=f"https://t.me/{app.username}?startgroup=true"
+            ),
+            InlineKeyboardButton(text="🍷Sᴜᴘᴘᴏʀᴛ🍷", url=config.SUPPORT_GROUP),
+        ]
+    ]
+    return buttons
+
+def private_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text="🥀ᴀᴅᴅ ᴍᴇ ɪɴ ɴᴇᴡ ɢʀᴏᴜᴘ🥀",
+                url=f"https://t.me/{app.username}?startgroup=true"
+            )
+        ],
+        [
+            InlineKeyboardButton(text="Uᴘᴅᴀᴛᴇ", url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text="Dᴇᴠᴇʟᴏᴘᴇʀ", user_id=config.OWNER_ID),
+            InlineKeyboardButton(text="Sᴜᴘᴘᴏʀᴛ", url=config.SUPPORT_GROUP),
+        ],
+        [
+            InlineKeyboardButton(text="Hᴇʟᴘ Aɴᴅ Cᴏᴍᴍᴀɴᴅs", callback_data="settings_back_helper"),
+        ]
+    ]
+    return buttons
