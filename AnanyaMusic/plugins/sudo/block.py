bb92858d93e83fce95ca6ea0d01f3483a9c4e4dd
AkashOP <akp954834@gmail.com>
2025-06-29 20:48:56 +0530

    Update block.py

--- AnanyaMusic/plugins/sudo/block.py
+++ AnanyaMusic/plugins/sudo/block.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ShrutiMusic import app
-from ShrutiMusic.misc import SUDOERS
-from ShrutiMusic.utils.database import add_gban_user, remove_gban_user
-from ShrutiMusic.utils.decorators.language import language
-from ShrutiMusic.utils.extraction import extract_user
+from AnanyaMusic import app
+from AnanyaMusic.misc import SUDOERS
+from AnanyaMusic.utils.database import add_gban_user, remove_gban_user
+from AnanyaMusic.utils.decorators.language import language
+from AnanyaMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
