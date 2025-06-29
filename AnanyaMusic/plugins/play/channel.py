e926fa6b8fbcc78993e58f433ec210c410ba20ff
AkashOP <akp954834@gmail.com>
2025-06-29 21:50:40 +0530

    Update channel.py

--- AnanyaMusic/plugins/play/channel.py
+++ AnanyaMusic/plugins/play/channel.py
@@ -2,9 +2,9 @@ from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter, ChatMemberStatus, ChatType
 from pyrogram.types import Message
 
-from ShrutiMusic import app
-from ShrutiMusic.utils.database import set_cmode
-from ShrutiMusic.utils.decorators.admins import AdminActual
+from AnanyaMusic import app
+from AnanyaMusic.utils.database import set_cmode
+from AnanyaMusic.utils.decorators.admins import AdminActual
 from config import BANNED_USERS
 
 
