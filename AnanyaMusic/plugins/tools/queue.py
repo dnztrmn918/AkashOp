9dc57d0f9a709f2db50b8af6e1837907caa93b43
AkashOP <akp954834@gmail.com>
2025-06-29 17:27:16 +0530

    Update queue.py

--- AnanyaMusic/plugins/tools/queue.py
+++ AnanyaMusic/plugins/tools/queue.py
@@ -6,12 +6,12 @@ from pyrogram.errors import FloodWait
 from pyrogram.types import CallbackQuery, InputMediaPhoto, Message
 
 import config
-from ShrutiMusic import app
-from ShrutiMusic.misc import db
-from ShrutiMusic.utils import AviaxBin, get_channeplayCB, seconds_to_min
-from ShrutiMusic.utils.database import get_cmode, is_active_chat, is_music_playing
-from ShrutiMusic.utils.decorators.language import language, languageCB
-from ShrutiMusic.utils.inline import queue_back_markup, queue_markup
+from AnanyaMusic import app
+from AnanyaMusic.misc import db
+from AnanyaMusic.utils import AviaxBin, get_channeplayCB, seconds_to_min
+from AnanyaMusic.utils.database import get_cmode, is_active_chat, is_music_playing
+from AnanyaMusic.utils.decorators.language import language, languageCB
+from AnanyaMusic.utils.inline import queue_back_markup, queue_markup
 from config import BANNED_USERS
 
 basic = {}
