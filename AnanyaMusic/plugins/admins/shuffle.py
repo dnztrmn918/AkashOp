8e5ddcdc62bf82552dec0f54f2bc403993193baa
AkashOP <akp954834@gmail.com>
2025-07-02 20:07:25 +0530

    Update shuffle.py

--- AnanyaMusic/plugins/admins/shuffle.py
+++ AnanyaMusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from ShrutiMusic import app
-from ShrutiMusic.misc import db
-from ShrutiMusic.utils.decorators import AdminRightsCheck
-from ShrutiMusic.utils.inline import close_markup
+from AnanyaMusic import app
+from AnanyaMusic.misc import db
+from AnanyaMusic.utils.decorators import AdminRightsCheck
+from AnanyaMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
