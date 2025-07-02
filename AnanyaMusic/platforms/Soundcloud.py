44835a78bfa350dca70a77db79c98de93307e22d
AkashOP <akp954834@gmail.com>
2025-07-02 20:31:52 +0530

    Update Soundcloud.py

--- AnanyaMusic/platforms/Soundcloud.py
+++ AnanyaMusic/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from ShrutiMusic.utils.formatters import seconds_to_min
+from AnanyaMusic.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
