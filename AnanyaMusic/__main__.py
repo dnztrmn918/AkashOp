825831cf9d81671a17d55778046a0bc10f125138
𝐍𝐚𝐧𝐝 𝐘𝐚𝐝𝐮𝐰𝐚𝐧𝐬𝐡𝐢 : NoxxOP <duhgfhfdghg+A1@gmail.com>
2025-07-03 06:24:36 +0530

    Update __main__.py

--- AnanyaMusic/__main__.py
+++ AnanyaMusic/__main__.py
@@ -1,62 +1,62 @@
-import asyncio 
-import importlib 
- 
-from pyrogram import idle 
-from pytgcalls.exceptions import NoActiveGroupCall 
- 
-import config 
-from AnanyaMusic import LOGGER, app, userbot 
-from AnanyaMusic.core.call import Aviax 
-from AnanyaMusic.misc import sudo 
-from AnanyaMusic.plugins import ALL_MODULES 
-from AnanyaMusic.utils.database import get_banned_users, get_gbanned 
-from config import BANNED_USERS 
- 
- 
-async def init(): 
-    if ( 
-        not config.STRING1 
-        and not config.STRING2 
-        and not config.STRING3 
-        and not config.STRING4 
-        and not config.STRING5 
-    ): 
-        LOGGER(__name__).error("Assistant client variables not defined, exiting...") 
-        exit() 
-    await sudo() 
-    try: 
-        users = await get_gbanned() 
-        for user_id in users: 
-            BANNED_USERS.add(user_id) 
-        users = await get_banned_users() 
-        for user_id in users: 
-            BANNED_USERS.add(user_id) 
-    except: 
-        pass 
-    await app.start() 
-    for all_module in ALL_MODULES: 
-        importlib.import_module("AnanyaMusic.plugins" + all_module) 
-    LOGGER("AnanyaMusic.plugins").info("Successfully Imported Modules...") 
-    await userbot.start() 
-    await Aviax.start() 
-    try: 
-        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4") 
-    except NoActiveGroupCall: 
-        LOGGER("AnanyaMusic").error( 
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..." 
-        ) 
-        exit() 
-    except: 
-        pass 
-    await Aviax.decorators() 
-    LOGGER("AnanyaMusic").info( 
-        "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73" 
-    ) 
-    await idle() 
-    await app.stop() 
-    await userbot.stop() 
-    LOGGER("AnanyaMusic").info("Stopping Ananya Music Bot...") 
- 
- 
-if __name__ == "__main__": 
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from AnanyaMusic import LOGGER, app, userbot
+from AnanyaMusic.core.call import Aviax
+from AnanyaMusic.misc import sudo
+from AnanyaMusic.plugins import ALL_MODULES
+from AnanyaMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("AnanyaMusic.plugins" + all_module)
+    LOGGER("AnanyaMusic.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Aviax.start()
+    try:
+        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("AnanyaMusic").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Aviax.decorators()
+    LOGGER("AnanyaMusic").info(
+        "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("AnanyaMusic").info("Stopping Ananya Music Bot...")
+
+
+if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
